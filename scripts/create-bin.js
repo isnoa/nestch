7bbb1be22d9de4ba3ed163b65901ff0670d05cfc
isnoa <isnoa3166@gmail.com>
2025-06-06 20:00:57 +0900

    fix: Update string formatting for content in create-bin.js

--- scripts/create-bin.js
+++ scripts/create-bin.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const binDir = path.join(__dirname, "../dist/bin");
 const binFile = path.join(binDir, "cli.js");
-const content = '#!/usr/bin/env node\nrequire(\"../lib/index\");';
+const content = `#!/usr/bin/env node\nrequire('../lib/index');`;
 
 fs.mkdirSync(binDir, { recursive: true });
 fs.writeFileSync(binFile, content, { encoding: "utf8" });
